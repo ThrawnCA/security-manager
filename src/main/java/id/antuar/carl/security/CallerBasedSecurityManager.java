e61dd775d321ae374b54e660ca56f05759b9b83f
ThrawnCA <thrawn@xanadu>
2014-08-27 06:30:53 +1000

    improve 'permissions needed' logging
    - configure with runtime flag
    - format like policy file
    TODO remove brackets from permission toString

--- src/main/java/id/antuar/carl/security/CallerBasedSecurityManager.java
+++ src/main/java/id/antuar/carl/security/CallerBasedSecurityManager.java
@@ -2,10 +2,14 @@ package id.antuar.carl.security;
 
 import java.security.AccessControlException;
 import java.security.Permission;
+import java.security.ProtectionDomain;
 import java.util.Arrays;
 import java.util.Collection;
 
 public class CallerBasedSecurityManager extends SecurityManager {
+
+	private static final boolean LOG_MODE = System.getProperty("java.security.manager.log_mode") != null;
+	private static final String PERMISSION_FORMAT = "grant codeBase \"%s\" {\n  permission %s\n} // (%s)";
 
 	private Collection<String> SYSTEM_PACKAGES = Arrays.asList("java.", "sun.");
 
@@ -41,9 +45,13 @@ public class CallerBasedSecurityManager extends SecurityManager {
 		if (clazz == null) {
 			return;
 		}
-		if (!clazz.getProtectionDomain().implies(perm)) {
-			throw new AccessControlException("access denied for "+clazz+": "+perm, perm);
+		ProtectionDomain domain = clazz.getProtectionDomain();
+		if (!domain.implies(perm)) {
+			if (LOG_MODE) {
+				System.err.println(String.format(PERMISSION_FORMAT, domain.getCodeSource().getLocation(), perm.toString().replace("\" \"", "\", \""), clazz.getName()));
+			} else {
+				throw new AccessControlException("access denied: "+perm, perm);
+			}
 		}
-		System.out.println("DEBUG: granting "+perm+" to "+clazz);
 	}
 }
